ef032e6457c0abc588f78c3665fcfa2bdef98375
wshafiqah <82049887+wshafiqah@users.noreply.github.com>
2021-04-07 13:09:08 +0800

    Add files via upload

--- charges.php
+++ charges.php
@@ -4,15 +4,48 @@ $hour = $_POST['hour'];
 $vehicle = $_POST['vehicle'];
 
 $totalcar = $hour*3;
-$totalmotor = $hour*1.5;
+$totalmotor = $hour*2;
 
 if ($vehicle = "car" && $totalcar <= 12){
-echo "Total Amount : " . $totalcar;
+echo "Please Pay Total Amount of Parking Fee: RM " . $totalcar;
 } 
 elseif ($vehicle = "motor" && $totalmotor <= 6){
-echo "Total Amount : " . $totalmotor;}
+echo "Please Pay Total Amount of Parking Fee: RM " . $totalmotor;}
 else{
-echo "Exceed maximum fee. Please pay RM50";
+echo "Exceed maximum fee. Please pay RM 50";
 }
+
+$level1car = [
+    ['vehicle' => 'car', 'level' => 1],
+    ['vehicle' => 'car', 'level' => 1],
+    ['vehicle' => 'car', 'level' => 1],
+    ['vehicle' => 'car', 'level' => 1],
+    ['vehicle' => 'car', 'level' => 1],
+    ['vehicle' => 'car', 'level' => 1]
+    ];
+ 
+$level1motor = [
+      ['vehicle' => 'motorcycle', 'level' => 1],
+      ['vehicle' => 'motorcycle', 'level' => 1],
+      ['vehicle' => 'motorcycle', 'level' => 1],
+      ['vehicle' => 'motorcycle', 'level' => 1],
+      ['vehicle' => 'motorcycle', 'level' => 1]
+      ];
+$level2 = [
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2],
+ ['vehicle' => 'car', 'level' => 2]
+ ];
+
+$totalvehicle = count($level1car)+count($level1motor)+count($level2);
+
+echo "<br>"."<br>"."Total Vehicle in Level 1 (Car) : " . count($level1car) ." / 15". "<br>" . "<br>" . "Total Vehicle in Level 1 (Motorcycle) : " . count($level1motor) ." / 15". "<br>" . "<br>" .
+"Total Vehicle in Level 2 : " . count($level2) ." / 30". "<br>" . "<br>" ."Sum of Vehicle : " . $totalvehicle ." / 60";
 
   ?>
